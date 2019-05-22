dffd1d537ee8913a852dd60abbea48adee34e91a
Juan José Nieves Seoane <juan95ns@gmail.com>
2019-05-22 18:53:52 +0200

    Django

---
+++ Code/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\juan-\Documents\Ing informática\LinuxHa-Clusters-Interface\Code\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
