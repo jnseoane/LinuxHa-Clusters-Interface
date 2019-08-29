cb1e2902ed27fbf84b58c3fb5ef5bf9470cb5dd4
Juan José Nieves Seoane <juan95ns@gmail.com>
2019-08-29 20:07:26 +0200

    Node status y agents refresh

--- cluster/urls.py
+++ cluster/urls.py
@@ -5,7 +5,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('nodes', views.nodes, name='nodes'),
+    path('nodes/<node_name>/online', views.nodes_online, name='nodes_online'),
+    path('nodes/<node_name>/standby', views.nodes_standby, name='nodes_standby'),
+    path('nodes/<node_name>/maintenance', views.nodes_maintenance, name='nodes_maintenance'),
+    path('nodes/<node_name>/ready', views.nodes_ready, name='nodes_ready'),
     path('resources', views.resources, name='resources'),
     path('nodes/<int:node_id>/resources', views.node_resources, name='node-resources'),
     path('agents', views.agents, name='agents'),
+    path('agents/<agent_name>/details', views.agent_details, name='agent_details'),
+    path('refresh', views.agents_refresh, name='refresh'),
 ]
--- cluster/views.py
+++ cluster/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 import subprocess
@@ -45,10 +45,28 @@ def nodes(request):
         'nodes': nodes,
     }
 
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
+
+def nodes_online(request, node_name):
+    subprocess.run(["crm", "node", "online", node_name], stdout=subprocess.PIPE)
+    return redirect('nodes')
+
+def nodes_standby(request, node_name):
+    subprocess.run(["crm", "node", "standby", node_name], stdout=subprocess.PIPE)
+    return redirect('nodes')
+
+def nodes_maintenance(request, node_name):
+    subprocess.run(["crm", "node", "maintenance", node_name], stdout=subprocess.PIPE)
+    return redirect('nodes')
+
+def nodes_ready(request, node_name):
+    subprocess.run(["crm", "node", "ready", node_name], stdout=subprocess.PIPE)
+    return redirect('nodes')
 
 def resources(request):
-    return HttpResponse("Resources")
+    context = {}
+    return render(request, 'resources.html', context)
 
 def node_resources(request, node_id):
     return HttpResponse("Resources of node: %s" % node_id)
@@ -60,33 +78,9 @@ def agents(request):
     agents = Agent.objects.all()
     params = Agent_Param.objects.all()
 
+    #Si no detecta ningún agente en la base de datos, carga todos los agentes de los .json a la base de datos
     if not agents:
-        #Si no hay ningún agente, accedemos a la carpeta donde con la aplicación previa del script, tenemos todos los .json y creamos las entidades
-        files = []
-        for r, d, f in os.walk('cluster/json'):
-            for file in f:
-                if '.json' in file:
-                    files.append(os.path.join(r, file))
-
-        #Por cada archivo json, lo abriremos y crearemos la entidad en la base de datos
-        #Eliminamos la primera linea, que viene con la ruta del ocf
-        for file in files:
-            with open(file, "r") as f:
-                str = f.read().split("\n",1)[1]
-                agent = json.loads(str)
-                a = Agent(name=agent["name"], resource_str=agent["resource"], title= agent["title"], description=agent["description"])
-                a.save()
-                for param in agent["params"]:
-                    p = Agent_Param(agent=a, name=param["name"], title=param["title"], description=param["descripion"],required=param["required"],unique=param["unique"], content_type=param["content_type"], default_value=param["default_value"])
-                    p.save()
-
-        agents = Agent.objects.all()
-        params = Agent_Param.objects.all()
-
-
-    else:
-        #Si tenemos las entidades las formateamos si hace falta
-        pass
+        agents, params = ra_json_refresh()
 
 
     context = {
@@ -94,3 +88,58 @@ def agents(request):
         'agent_params': params
     }
     return HttpResponse(template.render(context, request))
+
+def agents_refresh(request):
+    template = loader.get_template('agents.html')
+
+    agents, params = ra_json_refresh()
+
+    context = {
+        'agents': agents,
+        'agent_params': params
+    }
+    return redirect('agents')
+    #return HttpResponse(template.render(context, request))
+
+
+def agent_details(request, agent_name):
+    return redirect('agents')
+
+def ra_json_refresh():
+    agents_db = Agent.objects.all()
+
+    files = []
+    for r, d, f in os.walk('cluster/json'):
+        for file in f:
+            if '.json' in file:
+                files.append(os.path.join(r, file))
+
+    # Por cada archivo json, lo abriremos y crearemos la entidad en la base de datos
+    # Eliminamos la primera linea, que viene con la ruta del ocf
+    for file in files:
+        with open(file, "r") as f:
+            str = f.read().split("\n", 1)[1]
+            agent = json.loads(str)
+
+            #Comprobamos que no esté ya en la DB
+            new = True
+            for agent_db in agents_db:
+                if agent_db.name == agent["name"]:
+                    new = False
+                    break
+
+            #Si es nuevo, se introduce
+            if new:
+                a = Agent(name=agent["name"], resource_str=agent["resource"], title=agent["title"],
+                          description=agent["description"])
+                a.save()
+                for param in agent["params"]:
+                    p = Agent_Param(agent=a, name=param["name"], title=param["title"], description=param["descripion"],
+                                    required=param["required"], unique=param["unique"], content_type=param["content_type"],
+                                    default_value=param["default_value"])
+                    p.save()
+
+    agents = Agent.objects.all()
+    params = Agent_Param.objects.all()
+
+    return agents, params
